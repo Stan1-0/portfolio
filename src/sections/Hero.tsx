6cb9f61760688b1d5ed239fe4df113d5f11b6264
Stan1-0 <stankofb@gmail.com>
2025-01-31 15:38:46 +0000

    added rings to hero

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -6,8 +6,14 @@ import grainImage from '@/assets/images/grain.jpg'
 export const HeroSection = () => {
   return (
     <div className="py-32 md:py-48 lg:py-60 relative z-0" >
-      <div className="absolute inset-0" style={{
-         backgroundImage: `url(${grainImage.src})`}}>
+      <div className="absolute inset-0 -z-30 opacity-5 " style={{
+        backgroundImage: `url(${grainImage.src})`}}>
+        </div>
+        <div className=" size-[620px] hero-ring"></div>
+        <div className=" size-[820px] hero-ring"></div>
+        <div className=" size-[1020px] hero-ring"></div>
+        <div className=" size-[1220px] hero-ring"></div>
+      <div className="container">
         <div className="flex flex-col items-center">
           <Image
             src={memojiImage}
