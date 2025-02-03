c2b1f4918997dcb574f763ae7733ffc042691be3
Stan1-0 <stankofb@gmail.com>
2025-02-03 14:49:41 +0000

    created a card component

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -2,6 +2,9 @@ import { SectionHeader } from "@/components/SectionHeader";
 
 export const AboutSection = () => {
   return <div>
-    <SectionHeader />
+    <SectionHeader 
+    eyebrow="About Me" 
+    title="A Glimpse into my world"
+    description="Learn more about who I am, what I do, and what inspires me" />
   </div>;
 };
